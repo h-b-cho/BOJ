d1ef6b4337379e4b25ca71ef6c780feb102cd4fa
Haebin <hbtopkr@gmail.com>
2023-03-14 14:30:50 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 216 ms, Memory: 35676 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
@@ -1,37 +1,27 @@
+# 01 
 import sys
 import heapq
 input = sys.stdin.readline
 N = int(input())
 cards = list(int(input()) for _ in range(N))
-h1 = []
-h2 = []
+h = []
 tmph = []
 tmp0 = 0
 tot = 0 
 for c in cards:
-    if len(h1)==len(h2):
-        heapq.heappush(h1, c)
-    else:
-        heapq.heappush(h2, c)
-    if h1 and h2 and h2[0]>h1[0]:
-        tar_1_to_2_value = heapq.heappop(h2)
-        tar_2_to_1_value = heapq.heappop(h1)
-        heapq.heappush(h2, tar_2_to_1_value)
-        heapq.heappush(h1, tar_1_to_2_value)
-# 언제나 len(h1)>=len(h2)이 된다.
+    heapq.heappush(h, c)
 
-for i in range(len(h1)): heapq.heappush(tmph, h1[i])
-for i in range(len(h2)): heapq.heappush(tmph, h2[i])
-# 힙 tmph은 힙 h1과 힙 h2을 합친 힙이다.
+for i in range(len(h)): heapq.heappush(tmph, h[i])
+# 힙 tmph은 현재 힙 h과 동일한 힙이다.
 
 while len(tmph)>1:
     if N==1:
         tot = 0
         break
     try:
-        if int(len(h1)+len(h2))==N:
-            tmp1 = heapq.heappop(h1)
-            tmp2 = heapq.heappop(h2)
+        if int(len(h))==N:
+            tmp1 = heapq.heappop(h)
+            tmp2 = heapq.heappop(h)
             tot += tmp1+tmp2
             heapq.heappop(tmph)
             heapq.heappop(tmph)
