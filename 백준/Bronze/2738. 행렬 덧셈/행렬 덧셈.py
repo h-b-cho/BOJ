ded581ce16823d4303007b884be4c2f52638cd46
Haebin <hbtopkr@gmail.com>
2023-03-16 09:49:13 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -1,16 +1,17 @@
-A, B = [], []
-
+import sys
+input = sys.stdin.readline
 N, M = map(int, input().split())
-
+A = []
+B = []
 for row in range(N):
     row = list(map(int, input().split()))
     A.append(row)
+for col in range(N): # M이 아니다.
+    col = list(map(int, input().split()))
+    B.append(col)
 
-for row in range(N):
-    row = list(map(int, input().split()))
-    B.append(row)
-    
-for row in range(N):
-    for col in range(M):
-        print(A[row][col] + B[row][col], end=' ')
-    print()
+for r in range(N):
+    row = ''
+    for c in range(M):
+        row += (str(A[r][c] + B[r][c])+' ')
+    print(row)
