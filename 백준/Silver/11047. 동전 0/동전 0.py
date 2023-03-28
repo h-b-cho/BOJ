4a53a8604144439916d07822f19ccecdae175a0c
Haebin <hbtopkr@gmail.com>
2023-03-29 02:19:51 +0900

    [Silver IV] Title: 동전 0, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+N, K = map(int, input().split())
+coins = []
+for _ in range(N):
+	c = int(input())
+	coins.append(c)
+
+price = K
+cnt = 0
+
+for i in range(len(coins)):
+    curr_coin = coins[len(coins)-i-1]
+    while price//curr_coin >= 1:
+        cnt += price//curr_coin
+        price -= (price//curr_coin)*curr_coin
+        if price==0:
+            break
+
+print(cnt)
