cd6c00d21088175e7fc92b3cec5d6a2bd6a7a0e3
Haebin <hbtopkr@gmail.com>
2023-03-31 10:51:07 +0900

    [Silver IV] Title: 카드 놓기, Time: 44 ms, Memory: 31768 KB -BaekjoonHub

---
+++ 백준/Silver/5568. 카드 놓기/카드 놓기.py
@@ -0,0 +1,31 @@
+import sys
+from itertools import permutations
+input = sys.stdin.readline
+N = int(input().strip())
+K = int(input().strip())
+cards = []
+for _ in range(N): 
+    cards.append(str(input().strip()))
+
+# 정답 02 순열 구현 - 재귀함수 사용
+arr = [0]*N
+num = ''
+tmp = ''
+result = set()
+
+def permu(n): 
+    global num 
+    if n==K:
+        result.add(num) 
+        return
+    for i in range(N):
+        if arr[i]==0:
+            arr[i] = 1
+            tmp = cards[i]
+            num += tmp
+            permu(n+1)
+            arr[i] = 0
+            num = num[:-len(tmp)]
+
+permu(0)
+print(len(result))
